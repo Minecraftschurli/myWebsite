b2b8920e8992464c02522029c8e49a9f4418f404
Minecraftschurli <minecraftschurli@gmail.com>
2019-05-05 19:53:24 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-#sudo apt-get -y update
-#sudo apt-get -y upgrade
-#sudo apt-get -y install screen
-sudo pip3 install flask
-sudo pip3 install flask-bcrypt
-sudo pip3 install meinheld
-sudo pip3 install netaddr
-sudo pip3 install screenutils
-sudo pip3 install --upgrade tensorflow
-#echo "cd ~/webapp" >> ~/.bashrc
-#echo "screen -UR webapp -X sudo bash ./launch.sh" >> ~/.bashrc
-#sudo reboot
+cd /home/pi/webapp
+sudo su
+source /home/pi/opencv/OpenCV-master-py3/bin/activate
+pip3 install flask
+pip3 install flask-bcrypt
+pip3 install flask-login
+pip3 install meinheld
+pip3 install netaddr
+pip3 install --upgrade tensorflow
